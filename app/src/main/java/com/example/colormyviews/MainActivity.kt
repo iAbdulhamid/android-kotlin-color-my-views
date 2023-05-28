511b0d971122a11455f5f5f1ac289452be8095d8
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2023-05-28 21:32:06 +0200

    Exercise: Add Aligned Boxes with Click Handlers

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,5 +18,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            findViewById(R.id.txt_box_one),
+            findViewById(R.id.txt_box_two),
+            findViewById(R.id.txt_box_three),
+            findViewById(R.id.txt_box_four),
+            findViewById(R.id.txt_box_five),
+            findViewById(R.id.const_layout)
+        )
+
+        for (view in clickableViews) {
+            view.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.txt_box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.txt_box_two -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.txt_box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.txt_box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.txt_box_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
