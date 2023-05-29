92bd96d43a3821853cad4d92c959822e355b1c23
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2023-05-29 21:24:59 +0200

    Exercise: Add Baseline Constraint and Button Chain

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,7 +30,10 @@ class MainActivity : ComponentActivity() {
             findViewById(R.id.txt_box_three),
             findViewById(R.id.txt_box_four),
             findViewById(R.id.txt_box_five),
-            findViewById(R.id.const_layout)
+            findViewById(R.id.const_layout),
+            findViewById(R.id.btn_red),
+            findViewById(R.id.btn_yellow),
+            findViewById(R.id.btn_green)
         )
 
         for (view in clickableViews) {
@@ -50,6 +54,11 @@ class MainActivity : ComponentActivity() {
             R.id.txt_box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.txt_box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.txt_box_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.btn_red -> findViewById<TextView>(R.id.txt_box_three).setBackgroundResource(R.color.red)
+            R.id.btn_yellow -> findViewById<TextView>(R.id.txt_box_four).setBackgroundResource(R.color.yellow)
+            R.id.btn_green -> findViewById<TextView>(R.id.txt_box_five).setBackgroundResource(R.color.green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
